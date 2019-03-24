84c93d2c484daf5f69b2aed6463249027997db96
Luthfi Eko Trinowo <18217018@std.stei.itb.ac.id>
2019-03-24 22:04:45 +0700

    Card Update

--- ChanceCard.java
+++ ChanceCard.java
@@ -7,24 +7,50 @@ public class ChanceCard {
     ArrayList<String> chanceLIst = new ArrayList<String>();
     
     public ChanceCard() {
-        chanceLIst.add("Masuk Penajara");
-        chanceLIst.add("Maju 3 Langkah");
-        chanceLIst.add("Mundur 3 Langkah");
-        chanceLIst.add("Bayar Zakat 10%");
-        chanceLIst.add("Pergi ke Start");
-        chanceLIst.add("Mendapat Sumbangan 20%");
-        chanceLIst.add("Maju 5 Langkah");
-        chanceLIst.add("Mundur 5 Langkah");
-        chanceLIst.add("Mundur 3 Langkah");
-        chanceLIst.add("Maju 5 Langkah");
+        chanceLIst.add("Masuk Penajara"); //1, 
+        chanceLIst.add("Maju 3 Langkah"); //2, position
+        chanceLIst.add("Mundur 3 Langkah"); //3, position
+        chanceLIst.add("Bayar Zakat 10%"); //4, outcome
+        chanceLIst.add("Pergi ke Start"); //5, position
+        chanceLIst.add("Mendapat Sumbangan 20%"); //6, income
+        chanceLIst.add("Maju 5 Langkah"); //7, position
+        chanceLIst.add("Mundur 5 Langkah"); //8, position
+        chanceLIst.add("Mundur 3 Langkah"); //9, position
+        chanceLIst.add("Maju 5 Langkah"); //10, position
     };
 
-    public int CCnum() {
+    public int getNum() {
         int n = rand.nextInt(10);
         return n;
     }
 
-    public void print() {
-        System.out.println(chanceCard.get(CCnum()));
+    public void print(int n) {
+        System.out.println(chanceCard.get(n));
+    }
+
+    public void chanceAction(int n, Player p) {
+        if (n == 1) {
+
+        } else if (n == 2) {
+            p.move(3);
+        } else if (n == 3) {
+            p.move(-3);
+        } else if (n == 4) {
+            int x = p.getMoney()*0.1;
+            p.pay(x);
+        } else if (n == 5) {
+
+        } else if (n == 6) {
+            int x = p.getMoney()*0.2;
+            p.receive(x);
+        } else if (n == 7) {
+            p.move(5);
+        } else if (n == 8) {
+            p.move(-5);
+        } else if (n == 9) {
+            p.move(-3);
+        } else if (n == 10) {
+            p.move(5);
+        } 
     }
 }
--- CommunityChest.java
+++ CommunityChest.java
@@ -7,24 +7,48 @@ public class CommunityChest {
     ArrayList<String> CommunityChest = new ArrayList<String>();
     
     public ChanceCard() {
-        chanceLIst.add("Mendapat Sumbangan 1000"); //in
-        chanceLIst.add("Pajak Negara 2000"); //out
-        chanceLIst.add("Hadiah Ulang tahun 1750"); //in
-        chanceLIst.add("Tagihan BPJS Bulanan 3500"); //out
-        chanceLIst.add("Beasiswa BukaPigur 10000"); //in
-        chanceLIst.add("Bayar UKT 8000"); //out
-        chanceLIst.add("Nemu Duit 2000"); //in
-        chanceLIst.add("Dipalak Preman 6500"); //out
-        chanceLIst.add("Transferan Orang Tua 5000"); //in
-        chanceLIst.add("Bayar Laundry 4000"); //out
+        chanceLIst.add("Mendapat Sumbangan 1000"); //1, in
+        chanceLIst.add("Pajak Negara 2000"); //2, out
+        chanceLIst.add("Hadiah Ulang tahun 1750"); //3, in
+        chanceLIst.add("Tagihan BPJS Bulanan 3500"); //4, out
+        chanceLIst.add("Beasiswa BukaPigur 10000"); //5, in
+        chanceLIst.add("Bayar UKT 8000"); //6, out
+        chanceLIst.add("Nemu Duit 2000"); //7, in
+        chanceLIst.add("Dipalak Preman 6500"); //8, out
+        chanceLIst.add("Transferan Orang Tua 5000"); //9, in
+        chanceLIst.add("Bayar Laundry 4000"); //10, out
     };
 
-    public int CCnum() {
+    public int getNum() {
         int n = rand.nextInt(10);
         return n;
     }
 
-    public void print() {
-        System.out.println(CommunityChest.get(CCnum()));
+    public void print(int n) {
+        System.out.println(CommunityChest.get(n));
+    }
+
+    public void chanceAction(int n, Player p) {
+        if (n == 1) {
+            p.receive(1000);
+        } else if (n == 2) {
+            p.pay(2000);
+        } else if (n == 3) {
+            p.receive(1750);
+        } else if (n == 4) {
+            p.pay(3500);
+        } else if (n == 5) {
+            p.receive(10000);
+        } else if (n == 6) {
+            p.pay(8000);
+        } else if (n == 7) {
+            p.receive(2000);
+        } else if (n == 8) {
+            p.pay(6500);
+        } else if (n == 9) {
+            p.receive(5000);
+        } else if (n == 10) {
+            p.pay(4000);
+        } 
     }
 }
