e572c865c584f1534e85fbe376f494d7e559e8f3
Luthfi Eko Trinowo <18217018@std.stei.itb.ac.id>
2019-03-29 08:34:44 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,12 +8,29 @@ public class Main {
         List<Place> place  = new ArrayList<Place>();
         List<Player> player  = new ArrayList<Player>();
         int d1, d2, nPlayer;
-        String name, cmd;
+        String pName, cmd;
         boolean nextP = false;
 
         //PenambahanMap
         
+        //Pembacaan jumlah player
+        nPlayer = s.nextInt();
+        for (int i = 1 ; i <= nPlayer ; i++) {
+            System.out.println("Nama Player Ke-"+i+":");
+            pName = s.next();
+            //player.add(new Player(pName));
+            //Player harusnya parameter nama
+		}
         
-        
+        //Instant Dadu
+        Dadu dadu = new Dadu();
+
+        //Iterasi permainan
+
+
+
+
+        //Tutup scanner (penting ga sih)
+        s.close();
     }
 }
