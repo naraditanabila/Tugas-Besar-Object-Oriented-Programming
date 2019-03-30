f9d668b72d3be42c7f9f72856d4549acef311dac
Luthfi Eko Trinowo <18217018@std.stei.itb.ac.id>
2019-03-30 21:47:35 +0700

    Update

--- Lot.java
+++ Lot.java
@@ -1,5 +1,5 @@
 public class Lot extends Property { //inheritance
-	public Lot (String name, int set, int type, int price){
+	public Lot (String name, int set, int price){
 		super(name,2,price,set);
 	}
 	
