f261a36a046ac3485b48c6a7281eb17ef1a9be91
Luthfi Eko Trinowo <18217018@std.stei.itb.ac.id>
2019-03-30 21:10:54 +0700

    Update Space.java

--- Space.java
+++ Space.java
@@ -1,23 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Space extends Place {	//Start, Free Parking, Penjara, Go to Jail, Tax, Chance Card, Community Chest
-	private int step;
 	private int cost;
+	List<Place> p = new ArrayList<Place>();
 	
-	public Space(String name, int step, int cost){
+	public Space(String name, int cost){
 		super(name,0);
-		this.step = step;
 		this.cost = cost;
 	}
 	
-	public int getStep(){
-		return this.step;
-	}
-	
-	public void setStep(int step){
-		this.step=step;
-	}
-	
+	//getter setter
 	public int getCost(){
 		return this.cost;
 	}
@@ -26,15 +20,21 @@ public class Space extends Place {	//Start, Free Parking, Penjara, Go to Jail, T
 		this.cost=cost;
 	}
 	
+	//ListArray
+	public void addP(Place x) {
+		p.add(x);
+	}
+	
 	//Space Effects
     public void placeAffect(Player p){
 			if (getName().equals("Start")) {
 				p.cekStart();
 			} else if (getName().equals("Free Parking")) {
 				Scanner s = new Scanner(System.in);
-				String plot = s.next();
-				//int index = place.indexOf(plot);
-				//p.setPos(index);
+				System.out.println("Masukkan angka 1-40 : ");
+				int plot = s.nextInt();
+				p.setPos(plot);
+				s.close();
 				placeAffect(p);
 			} else if (getName().equals("Penjara")) {
 				System.out.println ("Player "+ p.getID() + "hanya lewat penjara.");
