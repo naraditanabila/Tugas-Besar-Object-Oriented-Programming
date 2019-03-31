83c4e3a1cb43f7ab787dfb4f0f74d1b1079071f5
Luthfi Eko Trinowo <18217018@std.stei.itb.ac.id>
2019-03-31 19:32:48 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static synchronized void main(String[] args) {
         Scanner s = new Scanner(System.in);
         List<Place> place = new ArrayList<Place>();
-        List<Player> player = new ArrayList<Player>();
+		List<Player> player = new ArrayList<Player>();
+		Player player;
         int d1, d2, nPlayer;
         String pName, cmd;
+        boolean endGame = false;
         boolean nextP = false;
 
         //PenambahanMap
@@ -89,10 +91,69 @@ public class Main {
         ccList.add("Transferan Orang Tua 5000",new CommunityChest()); //9, in
         ccList.add("Bayar Laundry 4000",new CommunityChest()); //10, out
         //Iterasi permainan
+		
+		Timer t = new Timer();
+		Turn turn  = new Turn(nPlayer, t);
 
-
-
-
+		while (!endGame) {
+			//Gatau disini ada thread apa nggak
+			cmd = s.next();
+			if (cmd.equals("roll")) {
+				int index = turn.getPlayer();
+				if (player.get(index).getJail()) {
+					System.out.println("Silahkan pilih Bayar atau Dadu");
+					String x = s.next();
+					if (x.equals("Dadu")) {
+						d1 = dadu.getN1();
+						d2 = dadu.getN2();
+						System.out.println("Dadu yang anda dapatkan adalah " + d1 + " dan " + d2);
+						if (d1 == d2) {
+							player.get(index).outJail();
+							System.out.println("Selamat! anda keluar dari sini");
+						} else {
+							System.out.println("Dadu tidak sama");
+						}
+					} else if (x.equals("Bayar")) {
+						player.get(index).outJail();
+						System.out.println("Selamat! anda keluar dari sini");
+					}
+				} else {
+					boolean db = true;
+					int dbi = 1;
+					while (db && (dbi <= 3)) {
+						d1 = dadu.getN1();
+						d2 = dadu.getN2();
+						System.out.println("Dadu yang didapatkan: " + d1 + " dan " + d2);
+						System.out.println("Player bergerak sebanyak " + (d1+d2) + " kotak");
+						player.get(index).move(d1+d2);
+						int type = place.get(player.get(index).getPos()).getType();
+						if (type == 0) {
+							Space.placeAffect(player.get(index));
+						} else {
+							if (place.get(player.get(index).getPos()).getOwner() == player.get(index)) {
+								//Thread masuk disini, kalau sudah lebih dari 30 detik keluar dari while
+								cmd = s.next();
+								//Ini belum selesai
+							} else {	
+								Property.placeAffect(player.get(index));
+							}
+						}
+						if (d1 == d2){
+							dbi++;
+						} else {
+							db = false;
+						}
+					}
+					if (dbi > 3) {
+						player.get(index).inJail();
+					} else {
+						turn.nextPlayer();
+					}
+				}
+			} else if (cmd.equals("quit")) {
+				endGame = true;
+			}
+		}
         //Tutup scanner (penting ga sih)
         s.close();
     }
