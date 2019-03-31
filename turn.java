83e1b582e10eb082ff8d043772455fa22d220e87
Luthfi Eko Trinowo <18217018@std.stei.itb.ac.id>
2019-03-31 22:53:57 +0700

    Update turn.java

--- turn.java
+++ turn.java
@@ -1,28 +1,13 @@
-public class Turn extends Thread {
-	private Timer timer;
+public class Turn{
 	private int totalPlayer;
-	private int turn = 1;	//Initiate turn 
 	private int player = 0;		//Initiate player index
 
-	public Turn(int totalPlayer, Timer t) {
+	public Turn(int totalPlayer) {
 		this.totalPlayer = totalPlayer;
-		this.timer = t;
-	}
-
-	public int getTurn() {
-		return this.turn;
 	}
 
 	public int getPlayer() {
 		return this.player;
-	}
-
-	public int getTime() {
-		return this.timer.getTime();
-	}
-
-	public void setTurn(int turn) {
-		this.turn = turn;
 	}
 
 	public void setTotalPlayer(int totalPlayer) {
@@ -32,16 +17,4 @@ public class Turn extends Thread {
 	public void nextPlayer() {
 		this.player = (this.player + 1) % this.totalPlayer;
 	}
-
-	public void run() {
-        synchronized (timer) {
-            while (true) {
-                try {
-                    timer.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
 }
