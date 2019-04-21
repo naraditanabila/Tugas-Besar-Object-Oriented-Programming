3239cc259a4d5e34be50c8a43fd16ee6a99dc440
Ismail F Aziz <25928284+skykykykykykykykykykykys@users.noreply.github.com>
2019-04-22 04:50:59 +0700

    Update Monopoly.java

--- src/monopoly/Monopoly.java
+++ src/monopoly/Monopoly.java
@@ -6,21 +6,15 @@
 package monopoly;
 
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 
 
